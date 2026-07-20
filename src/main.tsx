9310e3e18d9db3d6d6a5df6d7750e3b3c18e65c8
devcq <codeeaq@gmail.com>
2026-07-20 22:30:50 +0530

    Fix visitor tracking not firing on project detail pages
    useVisitorTracking() was only invoked inside App, so the new /projects/:slug route (added for project galleries) bypassed analytics entirely. Moved the hook to the router root so it fires regardless of route.

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,25 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.tsx'
 import ProjectDetail from './pages/ProjectDetail.tsx'
+import { useVisitorTracking } from './hooks/useVisitorTracking'
 import './index.css'
 import './utils/palette'
+
+function AppRoutes() {
+  useVisitorTracking();
+
+  return (
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/projects/:slug" element={<ProjectDetail />} />
+    </Routes>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/projects/:slug" element={<ProjectDetail />} />
-      </Routes>
+      <AppRoutes />
     </BrowserRouter>
   </React.StrictMode>,
 )
